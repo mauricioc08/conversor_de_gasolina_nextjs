91df407261ac2477fca8ee6d84bf8997341d04af
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-05-03 18:46:37 -0300

    Conversor de Gasolina em Nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,139 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import Img from "../public/foto.jpg";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
+  const [valor1, setValor1] = useState("");
+  const [valor2, setValor2] = useState("");
+  const [result, setResult] = useState(0);
+
+  function mask(e, setValor) {
+    let v = e.target.value;
+
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d)(\d{8})$/, "$1.$2");
+    v = v.replace(/(\d)(\d{5})$/, "$1.$2");
+
+    v = v.replace(/(\d)(\d{2})$/, "$1,$2");
+
+    if (v.length > 5) {
+      return;
+    }
+
+    setValor(v);
+  }
+
+  function moneyToFloat(v) {
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d)(\d{2})$/, "$1.$2");
+    return parseFloat(v);
+  }
+
+  function handleCalc() {
+    const resultDiv = moneyToFloat(valor2) / moneyToFloat(valor1);
+    if (resultDiv >= 0.7) {
+      setResult(1);
+    } else {
+      setResult(2);
+    }
+    setValor1("");
+    setValor2("");
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="wrapper">
+      <Head>
+        <title>Conversor de Gasolina</title>
+      </Head>
+      <div className="bomb-photo">
+        <Image src={Img} alt="celular" />
+      </div>
+      <div className="description">
+        <div className="group-description">
+          <h3 className="not-account">
+            Qual combustivel está compensando usar?
+          </h3>
+          <p className="not-account">
+            A principal diferença entre os dois combustíveis está na proporção
+            entre o preço e o desempenho de cada um.
+            <br /> Esta calculadora compara o preço do litro do Etanol e da
+            Gasolina e mostra qual é o combustível mais vantajoso,
+            financeiramente, no momento.
+            <br />
+            <br />
+            O Etanol vale a pena quando custar até 70% do valor da Gasolina.
+            <br />
+          </p>
+          <p className="not-account">
+            Preencha os campos abaixo com os preços dos combustíveis que você
+            encontra hoje nos postos e clique no botão “calcular” para saber
+            qual é a melhor opção.
+          </p>
+        </div>
+        <div className="group-input">
+          <div className="group-input2">
+            <h3>Qual a melhor opção?</h3>
+            <h4>Calcule aqui!!</h4>
+            <div className="inputs">
+              <p>Etanol (preço por litro)</p>
+              <input
+                className="form-control"
+                type="text"
+                inputMode="numeric"
+                onChange={(event) => mask(event, setValor2)}
+                value={valor2}
+                id="Preco1"
+                placeholder="R$ 0.00"
+              />
+            </div>
+            <div className="inputs">
+              <p>Gasolina (preço por litro)</p>
+              <input
+                className="form-control2"
+                type="text"
+                inputMode="numeric"
+                onChange={(event) => mask(event, setValor1)}
+                value={valor1}
+                id="Preco2"
+                placeholder="R$ 0.00"
+              />
+            </div>
+            <button className="botao" onClick={handleCalc}>
+              Calcular
+            </button>
+          </div>
+
+          <div className="group-input3">
+            {result == 1 && (
+              <div className="message1">
+                <h2>Vale mais a pena usar Gasolina!!!</h2>
+              </div>
+            )}
+            {result == 2 && (
+              <div className="message2">
+                <h2>Vale mais a pena usar Etanol!!!</h2>
+              </div>
+            )}
+
+            <h3>Entenda como o cálculo é feito</h3>
+            <p>Dividimos o valor do litro do Etanol pelo da Gasolina.</p>
+            <br />
+            <p>
+              Quando o resultado é menor que 0,7, a recomendação é abastecer com
+              Etanol. Se maior, a recomendação é escolher a Gasolina.
+            </p>
+            <br />
+            <p>
+              {" "}
+              <b>Exemplo:</b> se o Etanol custa R$ 3,29 e a Gasolina R$ 4,92, o
+              resultado da divisão do primeiro pelo segundo é 0,67, menor que
+              0,7. Portanto, é mais vantajoso abastecer com Etanol.
+            </p>
+            <br />
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
